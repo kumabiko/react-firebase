1502438cec89c698e81110fef775420374966830
kumabiko <nousbear.dev@gmail.com>
2022-05-29 21:20:02 +0900

    App コンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect } from "react";
+import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
+import Auth from "./components/Auth";
+import Feed from "./components/Feed";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Firebaseのユーザーに対して変化があった時に呼び出される
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      // authUserが存在する時
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    // クリーンナップ関数　Appコンポーネントがアンマウントされた時に実行される
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
+  return (
+    <>
+      {user.uid ? (
+        <div className={styles.app}>
+          <Feed />
+        </div>
+      ) : (
+        <Auth />
+      )}
+    </>
+  );
+};
 
 export default App;
