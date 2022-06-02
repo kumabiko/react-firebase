80a81700ba42a442cd9a6acff53bd27be1f8448c
kumabiko <nousbear.dev@gmail.com>
2022-06-03 08:42:50 +0900

    パスワードリセット機能

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -14,6 +14,7 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
+import Modal from "@mui/material/Modal";
 
 // import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import SendIcon from "@mui/icons-material/Send";
@@ -25,6 +26,18 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme();
+
+// モーダルを真ん中に表示する
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 const Auth: React.FC = () => {
   const dispatch = useDispatch();
@@ -234,7 +247,12 @@ const Auth: React.FC = () => {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <span className={styles.login_reset}>Forgot password</span>
+                  <span
+                    className={styles.login_reset}
+                    onClick={() => setOpenModal(true)}
+                  >
+                    Forgot password
+                  </span>
                 </Grid>
                 <Grid item>
                   <span
@@ -256,6 +274,39 @@ const Auth: React.FC = () => {
                 SignIn with Google
               </Button>
             </form>
+
+            <Modal open={openModal} onClose={() => setOpenModal(false)}>
+              <Box
+                style={getModalStyle()}
+                sx={{
+                  outline: "none",
+                  position: "absolute",
+                  width: 400,
+                  borderRadius: 10,
+                  backgroundColor: "white",
+                  boxShadow: theme.shadows[5],
+                  padding: theme.spacing(10),
+                }}
+              >
+                <div className={styles.login_modal}>
+                  <TextField
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    type="email"
+                    name="email"
+                    label="Reset E-mail"
+                    value={resetEmail}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      setResetEmail(e.target.value);
+                    }}
+                  />
+                  <IconButton onClick={sendResetEmail}>
+                    <SendIcon />
+                  </IconButton>
+                </div>
+              </Box>
+            </Modal>
           </Box>
         </Grid>
       </Grid>
